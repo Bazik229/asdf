1e67eb20fb38c48ef92617334bc5df327d72cc4a
rustam <rustam@exemple.ru>
2018-08-06 23:45:13 +0400

    github

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,11 @@
 public class Calculate {
 
 	public static void main(String[] arg) {
-System.out.println("Calculate...");
+		System.out.println("Calculate...");
+		int first = Integer.value0f(arg[0]);
+		int second = Integer.value0f(arg[1]);
+		int summ = first + second;
+		System.out.println("sum " + summ);
+
 }
 }
